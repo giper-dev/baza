444de2010a94d9f9150e16ca844575e829fb9bf5
jin <jin@hyoo.ru>
2026-02-06 18:30:48 +0300

    sw fix

--- yard/yard.web.ts
+++ yard/yard.web.ts
@@ -1,3 +1,3 @@
 namespace $ {
-	$giper_baza_yard.masters_default.push( $mol_dom.document.location.origin + '/' )
+	if( $mol_dom.document ) $giper_baza_yard.masters_default.push( $mol_dom.document.location.origin + '/' )
 }
