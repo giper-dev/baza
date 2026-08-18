15ce64e08279880d79cbcebbe2fd9eb426b88677
jin <jin@hyoo.ru>
2026-08-18 20:15:06 +0300

    no clone stat on face sync

--- face/face.ts
+++ face/face.ts
@@ -95,7 +95,7 @@ namespace $ {
 		
 		/** Synchronize this clock with another. */
 		sync( right: $giper_baza_face_data ) {
-			if( right instanceof $giper_baza_face_map ) this.stat = right.stat.clone()
+			// if( right instanceof $giper_baza_face_map ) this.stat = right.stat.clone()
 			for( const [ peer, face ] of right ) {
 				this.peer_time( peer, face.time, face.tick )
 				this.peer_summ( peer, face.summ )
