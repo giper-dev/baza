b6867aeced8a4ef30c4af9e599912e4275e10351
jin <jin@hyoo.ru>
2026-08-25 00:54:28 +0300

    + uptime in menu

---
+++ app/stat/list/list.view.css.ts
@@ -0,0 +1,19 @@
+namespace $.$$ {
+	
+	$mol_style_define( $giper_baza_app_stat_list, {
+		
+		Menu: {
+			flex: {
+				basis: '20rem',
+			},
+		},
+		
+		Menu_link: {
+			justify: {
+				content: 'space-between',
+			},
+		},
+		
+	} )
+	
+}
