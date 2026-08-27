6a63fc48011120932fc30e11042bbf0b890b8b0a
nin-jin <sairi-na-tenshi@ya.ru>
2026-08-27 10:38:08 +0000

    Deploying to gh-pages from @ giper-dev/baza@7febeb028813bf9be260ffef0aa894baf756044c 🚀

--- web.js
+++ web.js
@@ -27173,7 +27173,7 @@ var $;
         $mol_style_define($giper_baza_app_stat_list, {
             Menu: {
                 flex: {
-                    basis: '25rem',
+                    basis: '30rem',
                 },
             },
             Menu_link: {
