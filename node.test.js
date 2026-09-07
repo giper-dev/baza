3cd1c9bea41a55a582de9f012a541d0af1c21afd
nin-jin <sairi-na-tenshi@ya.ru>
2026-09-07 19:15:15 +0000

    Deploying to gh-pages from @ giper-dev/baza@0e2215680256584867cb47faa2d20e95739677a3 🚀

--- node.test.js
+++ node.test.js
@@ -11181,7 +11181,7 @@ var $;
                 const tx = this.sides[0].open('read_only');
                 const data = tx.read();
                 tx.destructor();
-                this.pool().acquire(data.byteLength);
+                this.pool().acquire(Math.ceil(data.byteLength / 8) * 8);
                 return data;
             }
             catch (error) {
